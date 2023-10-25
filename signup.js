5d4fff6d9d2912d77de472ce9561c6e3a080fa5c
Austin McMaster <austinmcmaster4@yahoo.com>
2023-10-25 15:41:01 -0600

    Fixed Form Default problem

--- signup.js
+++ signup.js
@@ -3,3 +3,13 @@ function signup() {
     localStorage.setItem("username", username.value);
     window.location.href = "index.html";
 }
+
+const registration_form = document.getElementById("register_form");
+registration_form.addEventListener("submit", checkValidity)
+
+function checkValidity(event){
+    if(registration_form.checkValidity()){
+        event.preventDefault();
+        signup();
+    }
+}
