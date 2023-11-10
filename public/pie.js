54c22682361c693dc326e07973cc151d97cfefa8
Austin McMaster <austinmcmaster4@yahoo.com>
2023-11-10 01:43:38 -0700

    Update Pie Update time

--- public/pie.js
+++ public/pie.js
@@ -1,23 +1,45 @@
-const xVal = []
-const yVal = [30,50,1]
-const barCol = ["blue","brown"]
-
-new Chart("pieChart", {
-    type: "pie",
-    data: {
-        labels:xVal,
-        datasets:[{
-            backgroundColor:barCol,
-            data:yVal,
-            label:"bar Chart",
-            barThickness:40,
-        }]
-    },
-    options: {
-        title:{
-            display:true,
-            text: "Pie Chart",
-            fontsize: 40,
+async function makePie(){
+    let data = [];
+    try {
+        const response = await fetch('api/table');
+        data = await response.json();
+        
+    }catch {
+        const entryCache = localStorage.getItem('table');
+        if(entryCache){
+            data = JSON.parse(entryCache);
         }
     }
-})
+
+    let values = [];
+    let labels = [];
+    for(var i = 0; i < data.length; i++){
+        var point = data[i];
+        values.push(point.Time);
+        labels.push(point.Subject);
+
+    }
+
+    const colors = ["purple","blue","red","green","black","orange","yellow","white","pink"]
+
+    var chart = new Chart("pieChart", {
+        type: "pie",
+        data: {
+            labels:values,
+            datasets:[{
+                backgroundColor:colors,
+                data:values,
+            }],
+            labels:labels
+        },
+        options: {
+            title:{
+                display:true,
+                text: "Pie Chart",
+                fontSize: 25,
+            }
+        }
+    })
+}
+
+makePie();
