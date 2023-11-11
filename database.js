40af42590fc9439360625c024201a24b8fca2c2f
Austin McMaster <austinmcmaster4@yahoo.com>
2023-11-10 21:06:43 -0700

    Database Update

---
+++ database.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const leaderboardCollection = db.collection('leaderboard');
+const tableCollection = db.collection("entrytable");
+const userCollection = db.collection("user");
+
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
