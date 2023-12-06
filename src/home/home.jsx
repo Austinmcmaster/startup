3b8400998a07919b3abf3f4084b2a65b68118d50
Austin McMaster <austinmcmaster4@yahoo.com>
2023-12-05 18:13:18 -0700

    CSS and styling in react

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import './home.css'
 
 export function Home() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>Home displayed here</div>
+    <main className='HomePage'>
+      <table className="Leaderboard" id="leaderboard">
+        <thead id='header'>
+          <tr>
+            <th>Rank</th>
+            <th>User</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody id='leaderboard_scores'></tbody>
+      </table>
+
+      <table id="DataTable">
+        <thead>
+          <tr>
+              <th>Subject</th>
+              <th>Description</th>
+              <th>Time (Hours)</th>
+          </tr>
+        </thead>
+        <tbody id='time_entries'></tbody>
+      </table>
+
+      <div className="container">
+            <form id="time_form">
+                <h2>Time Entry</h2>
+                <label htmlFor="Subect">Subject</label>
+                <input type="text"id = "Subject" required = "text" placeholder="Enter a associated subject"/>
+                <label htmlFor="Time_in">Time In</label>
+                <input type="time" id = "Time_in" required="time" placeholder="Enter Time in"/>
+                <label htmlFor="Time_out">Time Out</label>
+                <input type="time" id = "Time_out" required="time" placeholder="Enter Time out"/>
+                <label htmlFor="sub_descript">Description</label>
+                <textarea name = "Description" id="descriptionbox" title="Description_Box" placeholder="Enter Description here." cols="30" rows="5"></textarea>
+                <button className = "entry_button" type="submit">Submit</button>
+            </form>
+
+            <div className="pieContainer">
+            <canvas id = "pieChart" width="400" height="400"></canvas>    
+            </div>
+      </div>
+
+      <div className="weblabel">
+        <label htmlFor="webchat">Web Chat</label>
+        </div>
+        <div className="webchat">
+            <textarea type="text" id="webchat"title="Description_Box" cols="50" rows="8">
+            </textarea>
+            <br/>
+            <input type="text" placeholder="Enter text here" id="web_message" required = "text"></input>
+            <button id="web_button">Send Message</button>
+        </div>
+
     </main>
   );
 }
--- src/userdata/userdata.jsx
+++ src/userdata/userdata.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 
 export function UserData() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>UserData displayed here</div>
+    <main>
+      <table id = "DataTable">
+        <thead>
+            <tr>
+                <th>Subject</th>
+                <th>Description</th>
+                <th>Time (Hours)</th>
+            </tr>
+        </thead>
+      </table>
+        <button id='tablebutton' onClick="clearTable()">Clear Data</button>
+        <div className="container">
+            <form id="time_form">
+                <h2>Time Entry</h2>
+                <label htmlFor="Subect">Subject</label>
+                <input type="text"id = "Subject" required = "text" placeholder="Enter a description for text."/>
+                <label htmlFor="Time_in">Time In</label>
+                <input type="time" id = "Time_in" required="time" placeholder="Enter Time in"/>
+                <label htmlFor="Time_out">Time Out</label>
+                <input type="time" id = "Time_out" required="time" placeholder="Enter Time out"/>
+                <textarea name = "Description" id="descriptionbox" title="Description_Box" placeholder="Enter Description here." cols="30" rows="5"></textarea>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     </main>
   );
 }
