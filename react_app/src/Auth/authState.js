dfb8a27b622fa8fd962a2adfdc42c5a71133fba4
Austin McMaster <austinmcmaster4@yahoo.com>
2023-12-11 18:14:29 -0700

    cleaning up files

--- react_app/src/Auth/authState.js
+++
@@ -1,5 +0,0 @@
-export class AuthState {
-  static Unknown = new AuthState('unknown');
-  static Authenticated = new AuthState('authenticated');
-  static Unauthenticated = new AuthState('unauthenticated');
-}
